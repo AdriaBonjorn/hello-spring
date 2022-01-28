071895998db4bb5c2971416da7a824b946ffa5db
Adria Bonjorn <aibr@gft.com>
2022-01-28 13:39:58 +0100

    Added Parametrized tests. I need to rebuild the main code to be able to operate floats.

--- src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
@@ -1,6 +1,8 @@
 package com.sinensia.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,6 +41,25 @@ class DemoProjectApplicationTests {
 	}
 
 	@Test
+	void rootTestMultiName(@Autowired TestRestTemplate restTemplate) {
+		String[] arr = {"Adria","Patata","Whisky"};
+		for(String name: arr) {
+			assertThat(restTemplate
+					.getForObject("/hello?name="+name, String.class))
+					.isEqualTo("Hello "+name+"!");
+		}
+	}
+
+	@Autowired TestRestTemplate restTemplate;
+	@ParameterizedTest
+	@ValueSource(strings={"Adria","Whisky","Daddy","Kiwi"})
+	void helloParamNames( String name){
+		assertThat(restTemplate
+				.getForObject("/hello?name="+name, String.class))
+				.isEqualTo("Hello "+name+"!");
+	}
+
+	@Test
 	void canAdd(@Autowired TestRestTemplate restTemplate) {
 		assertThat(restTemplate
 				.getForObject("/add?a=1&b=2", String.class))
@@ -60,16 +81,23 @@ class DemoProjectApplicationTests {
 	}
 
 	@Test
-	void canAddNulla(@Autowired TestRestTemplate restTemplate) {
+	void canAddNullA(@Autowired TestRestTemplate restTemplate) {
 		assertThat(restTemplate
 				.getForObject("/add?a=&b=2", String.class))
 				.isEqualTo("2");
 	}
 
 	@Test
-	void canAddNullb(@Autowired TestRestTemplate restTemplate) {
+	void canAddNullB(@Autowired TestRestTemplate restTemplate) {
 		assertThat(restTemplate
 				.getForObject("/add?a=1&b=", String.class))
 				.isEqualTo("1");
+	}
+
+	@Test
+	void canAddFraction(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate
+				.getForObject("/add?a=1.5&b=2", String.class))
+				.isEqualTo("3.5");
 	}
 }
